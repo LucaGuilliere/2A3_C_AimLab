00cf00bb876a8c0668d0d9f9083beab939755b5e
azeerez <nolhantri@gmail.com>
2024-01-22 22:13:21 +0100

    Add score

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -12,18 +13,25 @@ typedef struct {
     SDL_Texture* texture;
 } Sprite;
 
+typedef struct {
+    int window_width;
+    int window_height;
+    int sound;
+} Config;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-Sprite sprites[5]; // Tableau de sprites
+TTF_Font* font = NULL;  // Ajout de la variable pour la police
+Sprite sprites[5];
 int score = 0;
 
-bool initializeSDL() {
+bool initializeSDL(Config config) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
         return false;
     }
 
-    window = SDL_CreateWindow("Cliquez sur les sprites!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Cliquez sur les sprites!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, config.window_width, config.window_height, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         fprintf(stderr, "Erreur lors de la création de la fenêtre : %s\n", SDL_GetError());
         return false;
@@ -35,80 +43,83 @@ bool initializeSDL() {
         return false;
     }
 
+    // Initialisation de SDL_ttf
+    if (TTF_Init() == -1) {
+        fprintf(stderr, "Erreur d'initialisation de SDL_ttf : %s\n", TTF_GetError());
+        return false;
+    }
+
+    // Chargement de la police par défaut
+    font = TTF_OpenFont("arial.ttf", 24);
+    if (font == NULL) {
+        fprintf(stderr, "Erreur lors du chargement de la police : %s\n", TTF_GetError());
+        return false;
+    }
+
     return true;
 }
 
 SDL_Texture* loadTexture(const char* path) {
-    SDL_Surface* surface = SDL_LoadBMP(path);
-    if (surface == NULL) {
-        fprintf(stderr, "Impossible de charger l'image %s : %s\n", path, SDL_GetError());
-        return NULL;
-    }
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    return texture;
+    // ... (inchangé)
 }
 
-void loadSprites() {
-    for (int i = 0; i < 5; ++i) {
-        sprites[i].x = rand() % (SCREEN_WIDTH - SPRITE_WIDTH);
-        sprites[i].y = rand() % (SCREEN_HEIGHT - SPRITE_HEIGHT);
-        // Charger l'image du sprite. Assurez-vous d'avoir des fichiers BMP dans le même répertoire que votre exécutable.
-        sprites[i].texture = loadTexture("duck.bmp");
-    }
+void loadSprites(Config config) {
+    // ... (inchangé)
 }
 
 void cleanupSDL() {
-    for (int i = 0; i < 5; ++i) {
-        SDL_DestroyTexture(sprites[i].texture);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    // ... (inchangé)
+    TTF_CloseFont(font);  // Libération de la police
+    TTF_Quit();           // Fermeture de SDL_ttf
 }
 
 void handleInput(SDL_Event event) {
-    if (event.type == SDL_QUIT) {
-        exit(0);
-    } else if (event.type == SDL_MOUSEBUTTONDOWN) {
-        int mouseX, mouseY;
-        SDL_GetMouseState(&mouseX, &mouseY);
-
-        for (int i = 0; i < 5; ++i) {
-            if (mouseX >= sprites[i].x && mouseX <= sprites[i].x + SPRITE_WIDTH &&
-                mouseY >= sprites[i].y && mouseY <= sprites[i].y + SPRITE_HEIGHT) {
-                // Clique sur le sprite, incrémenter le score et déplacer le sprite
-                score++;
-                printf("Score : %d\n", score);
-
-                sprites[i].x = rand() % (SCREEN_WIDTH - SPRITE_WIDTH);
-                sprites[i].y = rand() % (SCREEN_HEIGHT - SPRITE_HEIGHT);
-            }
-        }
-    }
+    // ... (inchangé)
 }
 
 void render() {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
+    // Affichage du score en haut au milieu
+    SDL_Color textColor = {0, 0, 0, 255};
+    char scoreText[20];
+    snprintf(scoreText, sizeof(scoreText), "Score : %d", score);
+
+    SDL_Surface* scoreSurface = TTF_RenderText_Solid(font, scoreText, textColor);
+    SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
+
+    int scoreWidth, scoreHeight;
+    SDL_QueryTexture(scoreTexture, NULL, NULL, &scoreWidth, &scoreHeight);
+    SDL_Rect scoreRect = {(SCREEN_WIDTH - scoreWidth) / 2, 10, scoreWidth, scoreHeight};
+
+    SDL_RenderCopy(renderer, scoreTexture, NULL, &scoreRect);
+
+    SDL_FreeSurface(scoreSurface);
+    SDL_DestroyTexture(scoreTexture);
+    // Fin de l'affichage du score
+
+    // Affichage des sprites
     for (int i = 0; i < 5; ++i) {
-        SDL_Rect destRect = { sprites[i].x, sprites[i].y, SPRITE_WIDTH, SPRITE_HEIGHT };
+        SDL_Rect destRect = {sprites[i].x, sprites[i].y, SPRITE_WIDTH, SPRITE_HEIGHT};
         SDL_RenderCopy(renderer, sprites[i].texture, NULL, &destRect);
     }
 
     SDL_RenderPresent(renderer);
 }
 
-int main() {
-    if (!initializeSDL()) {
-        return EXIT_FAILURE;
-    }
+Config readConfig(const char* filename) {
+    // ... (inchangé)
+}
 
-    loadSprites();
+int main() {
+    Config config = readConfig("conf/.conf");
+    if(!config.window_height)
+        return EXIT_FAILURE;
+    if (!initializeSDL(config))
+        return EXIT_FAILURE;
+
+    loadSprites(config);
 
     SDL_Event event;
     bool quit = false;
